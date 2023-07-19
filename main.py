ebb2e20c351286a5a88e8ecbe56125d3037d56d6
Ben Schiller <benschiller@Bens-MacBook-Pro.local>
2023-07-19 13:04:45 -0600

    added decode function and comments - Ben Schiller

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 # 45678888
 
 # Rona Escano
-def encode(code1):
+def encode(code1):  # encoder - Rona Escano
     encoded = ''
     dcode = list(code1)
     for i in range(0, len(dcode)):
@@ -12,11 +12,19 @@ def encode(code1):
         encoded = encoded + value
     return encoded
 
+def decode(encoded):   # decoder - Ben Schiller
+    decoded = ''
+    dcode = list(encoded)
+    for i in range(0, len(encoded)):
+        value = str(int(dcode[i]) - 3)
+        decoded = decoded + value
+    return decoded
 
-if __name__ == "__main__":
+
+if __name__ == "__main__":  # main function - Rona Escano
 
     while True:
-        # print Menu
+        # print Menu - Rona Escano
         print()
         print("Menu")
         print("-------------")
@@ -25,16 +33,17 @@ if __name__ == "__main__":
         print("3. Quit")
         option = int(input("Please enter an option: "))
 
-        # encoder
+        # encoder - Rona Escano
         if option == 1:
             code = input("Please enter your password to encode: ")
             encode = encode(code)
             print("Your password has been encoded and stored!")
 
-        # decoder
+        # decoder - Ben Schiller
         if option == 2:
-            pass
+            decode = decode(encode)
+            print("The encoded password is", encode, "and the original password is", decode + ".")
 
-        # exit program
+        # exit program - Rona Escano
         if option == 3:
             exit()
